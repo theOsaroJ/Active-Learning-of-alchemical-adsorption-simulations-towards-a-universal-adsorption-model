6544d02714ab88906a43b9b6e9bf52182ea186d6
Osaro, 'Nosa James <64130121+theOsaroJ@users.noreply.github.com>
2023-09-04 13:45:52 -0400

    Create uge.sh

---
+++ Fugacity and Adsorbates Space/uge.sh
@@ -0,0 +1,20 @@
+### ----------------------------- Author: Etinosa Osaro, Resources: Center for Research Computing, University of Notre Dame-------------------------------------###
+
+### --------------------------------------------------- Objective ------------------------------------------------------------ ###
+## This script allows user to run all AL in all 1800 MOFs using different resources as specified in run.sh
+
+#!/bin/bash
+
+for d in $(cat mof.txt)
+do
+cd $d
+mkdir KernelCombination
+cd KernelCombination
+
+cp ../../GP.py .
+cp ../../run.sh .
+
+sed -i 's/INDEX/'$d'/' run.sh
+qsub run.sh
+cd ../../
+done
