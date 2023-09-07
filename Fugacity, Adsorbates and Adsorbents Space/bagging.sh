ff8ec239c0e9be535965fe415d5a9b96c913ddd5
Osaro, 'Nosa James <64130121+theOsaroJ@users.noreply.github.com>
2023-09-06 22:23:46 -0400

    Create bagging.sh

---
+++ Fugacity, Adsorbates and Adsorbents Space/bagging.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#$ -q hpc@@colon
+#$ -pe smp 24
+#$ -N bagging
+
+for i in $(cat mof.txt)
+do
+cd $i
+
+rm sfile_*
+
+cp ../edit.py .
+
+cp *.csv data.csv
+sed -i '1, 1d' data.csv
+
+python3 edit.py
+
+cd ..
+done
+
+for i in $(cat mof.txt)
+do
+
+cd $i
+
+for ((n=1; n<=50; n++)); do
+cat "sfile_$n.csv" >> "../data_$n.csv"
+done
+
+cd ../
+done
+
+mkdir Bagging
+mv data_* Bagging
+
+cd Bagging
+
+for ((n=1; n<=50; n++)); do
+cut -d, -f 28-35 data_$n.csv > chunk_$n.csv
+done
+
+mkdir Chunks
+mv chunk_* Chunks
